33803d9d6308125fc1240d686f9984498804ea7b
Jason Greenlees <jason@aaweb.com.au>
2021-05-10 17:28:42 +1000

    new updates 2

--- web/themes/custom/compony/components/_global/script.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  Drupal.behaviors.global = {
-    attach: function (context, settings) {
-      //alert('yes');
-    }
-  };
-})(jQuery);
--- web/themes/custom/compony/gulpfile.js/core/options/htmlhintrc.js
+++
@@ -1,6 +0,0 @@
-{
-  "doctype-first": false,
-  "doctype-html5": false,
-  "title-require": false,
-  "src-not-empty": false
-}
